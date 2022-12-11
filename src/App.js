2d7aa9aac4e1a6f1e6408265486dec3f9b9c4f0a
unknown <cesyrub@gmail.com>
2022-12-11 13:25:13 +0100

    add wysyng

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+
+function App() {
+	const [value, setValue] = useState("");
+
+	const quillModules = {
+		toolbar: [
+			[{ header: "1" }, { header: "2" }, { font: [] }],
+			[{ size: [] }],
+			["bold", "italic", "underline", "strike", "blockquote"],
+			[
+				{ list: "ordered" },
+				{ list: "bullet" },
+				{ indent: "-1" },
+				{ indent: "+1" },
+			],
+			["link", "image", "video"],
+			["clean"],
+		],
+		clipboard: {
+			// toggle to add extra line breaks when pasting HTML:
+			matchVisual: false,
+		},
+	};
+	return (
+		<div className="App w-full">
+			<div className="flex flex-col justify-center items-center">
+				<ReactQuill
+					theme="snow"
+					value={value}
+					modules={quillModules}
+					onChange={setValue}
+					style={{
+						width: "80%",
+						backgroundColor: "white",
+						marginTop: "20px",
+						height: "400px",
+					}}
+				/>
+
+				<div className="output pt-24 w-8/12 pb-24">{value}</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
